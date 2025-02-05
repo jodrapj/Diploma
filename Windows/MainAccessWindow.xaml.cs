4696ce7acbb26278ae4a2356482f6459fa740504
jodrapj <jodrapj@gmail.com>
2025-02-05 17:05:22 +0300

    update

--- Windows/MainAccessWindow.xaml.cs
+++ Windows/MainAccessWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using PRKTK030225.Classes;
 
 namespace PRKTK030225.Windows
@@ -8,12 +10,24 @@ namespace PRKTK030225.Windows
     /// </summary>
     public partial class MainAccessWindow : Window
     {
+        ProgressBarWindow progressBarWindow;
+
         public MainAccessWindow(int accessLevel, string login)
         {
             InitializeComponent();
+            progressBarWindow = new ProgressBarWindow("Закрываем соединение с БД.", "Подождите");
             Data.MFrame = MFrame;
             LoginShowBox.Text = $"Вы вошли как: {login}";
             VersionBox.Text = Data.version;
+            Data.MFrame.Navigate(new Pages.MainPage());
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            progressBarWindow.Show();
+            e.Cancel = true;
+            Connect.context.Database.Connection.Close();
+            Environment.Exit(0);
         }
     }
 }
