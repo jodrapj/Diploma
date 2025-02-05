864faabeabc69f1bbbc73dfcfa5a1fe80c178617
jodrapj <jodrapj@gmail.com>
2025-02-05 13:32:59 +0300

    init

---
+++ Classes/Nav.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PRKTK030225.Classes
+{
+    public class Nav
+    {
+        public static Frame MFrame;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using PRKTK030225.Classes;
+
+namespace PRKTK030225
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Nav.MFrame = MFrame;
+        }
+    }
+}
